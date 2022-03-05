f553d286ad687f43d87c3001295c82fcdbe807a7
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-03-05 12:58:04 +0200

    refactor CategoryCollectionViewCell.swift

--- aliasTeamApp/Controllers/CategoryCollectionViewCell.swift
+++ aliasTeamApp/Controllers/CategoryCollectionViewCell.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - IB Outlets
     @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var categoryLabel: UILabel!
     
+    // MARK: - Public Methodes
     func configure(nameCategory: String){
         self.categoryLabel.text = nameCategory
         self.categoryLabel.textColor = .white
         self.cellView.layer.cornerRadius = 20
     }
-    
-    
-    
 }
